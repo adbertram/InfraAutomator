656748af7c61a912b6bc52d57adc725fbefaee02
Adam Bertram <adbertram@gmail.com>
2020-05-24 10:47:07 -0500

    updates

--- demo.ps1
+++ demo.ps1
@@ -10,12 +10,16 @@ to configure the VMs and Pester to perform infrastructure testing.
 Important concepts to note:
 
 Public GitHub repo: https://github.com/adbertram/infraautomator
-AzDo pipeline: https://dev.azure.com/adbertram/Infrastructure%20Automator
+Blog post: https://adamtheautomator.com/azure-devops-pipeline-infrastructure
+https://adamtheautomator.com/terraform-azure/
 
 3 VMs:
  - VM-0 (DC)
  - VM-1 (WEBSRV)
  - VM-2 (APPSRV)
+
+
+
 
 Assumptions
 ============
@@ -23,33 +27,26 @@ Assumptions
 2. You have Terraform already on your machine.
 3. You're using PowerShell Core.
 4. You have the GitHub repo cloned to $repoWorkingDir
+5. You have a PAT generated in your GitHub repo (https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line#creating-a-token)
 
 WARNING: Using this setup exactly as is WILL incur some cost!
 
 #>
 
+## Test demo environment so everything is in place
 Invoke-Pester
 
 $repoWorkingDir = '/Users/adam/Dropbox/GitRepos/InfraAutomator'
-
-#region One-time local preparation tasks
-
-#region Create an Azure service principal for Terraform to use
-
-## Find your subscription ID
-az account list --query [*].[name, id]
-
-## Create the service principal with the Contributor role scoped to your subscription
-$subscriptionId = 'xxxx-xxxxx-xxxxx'
-$sp = az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$subscriptionId" -n InfrastructureAutomator | ConvertFrom-Json
-
-#endregion
-
-#endregion
+$azResourceGroup = 'dev_playground'
+$azRegion = 'eastus'
+$projectName = 'InfrastructureAutomator'
+$azDoOrgName = 'https://dev.azure.com/adbertram'
 
 #region Local development
 ## Here we will ensure we can build all of the necessary VMs, run the configuration and tests. Esentially, we're doing
 ## all of the steps locally before moving these into an AzDo pipeline
+
+#region Terraform configuration
 
 #region Download Azure RM Terraform provider
 Set-Location -Path $repoWorkingDir
@@ -60,16 +57,118 @@ terraform init
 terraform plan --var-file=secrets.tfvars
 #endregion
 
-#region Create the VMs with Terraform
+#region Create the infrastructure with Terraform
 terraform apply --var-file=secrets.tfvars
 #endregion
 
 #endregion
 
+#region PowerShell code and Pester tests
+
+## Here is where we will create and test the code to configure all of the VMs and build the infrastructure Pester tests
+
+## Connecting to each VM with PS Remoting
+
+## Configuring each VM
+
+## Pester tests the pipeline will eventually invoke automatically
+
+#endregion
+
+#endregion
+
+#region Moving all local dev efforts to AzDo
+
+#region Prep the local dev environment for the pipeline
+
+## Remove the secrets.tfvars because we'll be using the Azure Key Vault for that
+Remove-Item -Path "$repoWorkingDir\secrets.tfvars"
+
+## Update the main configuration to not include secrets
+
+#endregion
+
+#region Variable assignments
+
+## These vars will be used throughout to set everything up
+$azSubscriptionName = 'Adam the Automator'
+$azDoOrgName = 'https://dev.azure.com/adbertram'
+$azRegion = 'eastus'
+$projectName = 'InfrastructureAutomator'
+$azRgName = $projectName
+$azDoProjectName = $projectName
+$gitHubRepoUrl = 'https://github.com/adbertram/InfrastructureAutomator'
+$azPipelineName = $projectName
+
+az account list --query [*].[name, id]
+$azSubscriptionId = 'xxxx'
+$azTenantId = 'xxxxx'
+
+#endregion
+
+#region Install the Azure CLI DevOps extension to build AzDo from the command line
+az extension add --name azure-devops
+az devops configure --defaults organization=$azDoOrgName
+#endregion
+
+#region Create the resource group
+
+## This could be done with Terraform but we're going to add some resources to it that every deployment run will share
+az group create --location $azRegion --name $azRgName
+
+#endregion
+
+#region Create a service principal
+## This will be how the pipeline authenticates to your subscription
+$spIdUri = "http://$projectName"
+$sp = az ad sp create-for-rbac --name $spIdUri | ConvertFrom-Json
+#endregion
+
+#region Create the keyvault and add secrets
+## They keyvault holds all of the sensitive information the pipeline will use
+$kvName = "$projectName-KV"
+az keyvault create --location $region --name $kvName --resource-group $azRgName --enabled-for-template-deployment true
+
+## Create the secrets to store the VM username/password
+az keyvault secret set --name VMAdminUsername --value 'dev_admin' --vault-name $kvName
+az keyvault secret set --name VmAdminPassword --value "I like azure." --vault-name $kvName
+
+## allow the pipeline to access the key vault
+az keyvault set-policy --name $kvName --spn $spIdUri --secret-permissions get list
+
+#endregion
+
+#region Create the AzDo project
+az devops project create --name $azDoProjectName
+
+## Set the project default so we don't have to specify it every time
+az devops configure --defaults project=$azDoProjectName
+#endregion
+
+#region Install Terraform extension
+## https://marketplace.visualstudio.com/items?itemName=charleszipp.azure-pipelines-tasks-terraform
+az devops extension install --extension-id Terraform --publisher-id "Charles Zipp"
+#endregion
+
+#region Create service connection and configure pipeline to use
+## Run $sp.password and copy it to the clipboard
+$sp.Password
+
+## for the pipeline to auth to subscription
+az devops service-endpoint azurerm create --azure-rm-service-principal-id $sp.appId --azure-rm-subscription-id $azSubscriptionId --azure-rm-subscription-name $azSubscriptionName --azure-rm-tenant-id $azTenantId --name 'ARM'
+
+## pipeline to auth to GitHub
+$gitHubServiceEndpoint = az devops service-endpoint github create --github-url $gitHubRepoUrl --name 'GitHub' | ConvertFrom-Json
+
+#endregion
+
+#region Create the pipeline but don't run it yet
+az pipelines create --name $azPipelineName --repository $gitHubRepoUrl --branch master --service-connection $gitHubServiceEndpoint.id --skip-run
+#endregion
+
 #region Environment cleanup
 
 ## Remove the service principal
-## Is this necessary if using the Azure CLI????
 $spId = ((az ad sp list --all | ConvertFrom-Json) | Where-Object { 'http://InfrastructureAutomator' -in $_.serviceprincipalnames }).objectId
 az ad sp delete --id $spId
 #endregion
